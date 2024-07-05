292675cbee797664748de2ce466d612689cc88d0
pembattech <pembat.tech@gmail.com>
2024-07-05 06:57:31 +0545

    Update PHP

--- PHP/2.1. type_casting.php
+++ PHP/2.1. type_casting.php
@@ -1,15 +1,12 @@
 <?php
 
-$num = "123";  // String containing a number
+$num = "123";
 
-// Casting to integer using operator
-$int_num = (int) $num;  // $int_num will be 123 (integer)
+$int_num = (int) $num;
 
-// Casting to float
 $float_num1 = (float) $num;
 
-// Casting to boolean (true for non-zero values)
-$is_positive = (bool) $int_num;  // $is_positive will be true
+$is_positive = (bool) $int_num;
 
 
 ?>
